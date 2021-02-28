172387028b0eaac5557cb6e67d6ef9187a66a8f1
OSCAR RONALDO MENCOS CHAMALE <oscaronaldo2001@gmail.com>
2021-02-28 00:22:51 -0600

    Navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,9 @@ import {
   NavLogo,
   NavIcon,
   MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
 } from "./Navbar.elements";
 import { IconContext } from "react-icons/lib";
 const Navbar = () => {
@@ -14,19 +17,30 @@ const Navbar = () => {
   const handleClick = () => setClick(!click);
 
   return (
-      <>
-    <IconContext.Provider value={{ color: "#fff" }}>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">
-            <NavIcon />
-            WEB-SITE
-          </NavLogo>
-          <MobileIcon onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
-          </MobileIcon>
-        </NavbarContainer>
-      </Nav>
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              WEB-SITE
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/products">Products</NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
       </IconContext.Provider>
     </>
   );
